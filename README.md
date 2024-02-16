126a48bc5ae0e6eaea6c29cc457ebdcbb292b89b
aapozd <aapozdnyakove@gmail.com>
2024-02-16 23:53:57 +0300

    Refactor config
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dormyboba-core
 Dormyboba core service
+
+## Local development
+
+Change this line
+
+```
+CONFIG_DIR = Path(__file__).resolve().parent.parent / "config"
+```
--- dormyboba_core/__main__.py
+++ dormyboba_core/__main__.py
@@ -4,31 +4,20 @@ from pathlib import Path
 import asyncio
 from sqlalchemy import create_engine
 import gspread
+from .config import parse_config
 from .server import serve
 
 CONFIG_DIR = Path("/").resolve() / "config"
 
-config = None
-with open(CONFIG_DIR / "config.yaml", "r") as yamlfile:
-    config = yaml.load(yamlfile, Loader=yaml.FullLoader)["dormyboba"]
-
-pg_config = config["postgres"]
-PG_USER = pg_config["user"]
-PG_PASSWORD = pg_config["password"]
-PG_HOST = pg_config["host"]
-PG_DB = pg_config["db"]
-DB_URL = f"postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}/{PG_DB}"
-
-gc_config = config["gc"]
-DEFECT_SHEET_ID = gc_config["defect_sheet_id"]
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info("Creating engine...")
-    engine = create_engine(DB_URL)
+
+    config = parse_config(CONFIG_DIR / "config.yaml")
+    engine = create_engine(config.pg_config.db_url)
 
     gc = gspread.service_account(filename=CONFIG_DIR / "service_account.json")
-    defect_sheet = gc.open_by_key(DEFECT_SHEET_ID)
+    defect_sheet = gc.open_by_key(config.gsheet_config.defect_sheet_id)
     worksheet = defect_sheet.get_worksheet(0)
 
     asyncio.run(serve(engine, worksheet))
