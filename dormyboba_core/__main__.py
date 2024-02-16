757aae3a6b11330fbb32067e83d8f361481397f0
aapozd <aapozdnyakove@gmail.com>
2024-02-17 01:35:54 +0300

    First run using fastapi
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- dormyboba_core/__main__.py
+++ dormyboba_core/__main__.py
@@ -11,30 +11,44 @@ Options:
 """
 from docopt import docopt
 import warnings
+from typing import Any
 import logging
 import yaml
 from pathlib import Path
 import asyncio
 from sqlalchemy import create_engine
 import gspread
-from .config import parse_config
+import uvicorn
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from .config import parse_config, DormybobaConfig
 from .server import serve
 
 warnings.filterwarnings("ignore", category=SyntaxWarning)
 
-if __name__ == "__main__":
-    arguments = docopt(__doc__, version='Naval Fate 2.0')
-    config_dir = arguments["--config-dir"]
-    config_path = Path(config_dir).resolve()
+ARGUMENTS = docopt(__doc__, version='DormybobaCore')
+CONFIG_PATH = Path(ARGUMENTS["--config-dir"]).resolve()
+CONFIG = parse_config(CONFIG_PATH / "config.yaml")
 
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info("Creating engine...")
+ENGINE = create_engine(CONFIG.pg_config.db_url)
 
-    config = parse_config(config_path / "config.yaml")
-    engine = create_engine(config.pg_config.db_url)
-
-    gc = gspread.service_account(filename=(config_path / "service_account.json"))
+def get_worksheet(config: DormybobaConfig) -> None:
+    gc = gspread.service_account(filename=(CONFIG_PATH / "service_account.json"))
     defect_sheet = gc.open_by_key(config.gsheet_config.defect_sheet_id)
-    worksheet = defect_sheet.get_worksheet(0)
+    return defect_sheet.get_worksheet(0)
 
-    asyncio.run(serve(engine, worksheet))
+WORKSHEET = get_worksheet(CONFIG)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(serve(ENGINE, WORKSHEET))
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
