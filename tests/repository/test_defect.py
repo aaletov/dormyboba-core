3626ca73df8227d575b4fe5f25fd98e4576a5f0d
dlopatinsky <dmitrij.lopatinskij@gmail.com>
2024-02-20 11:55:57 +0300

    Implement another test for defect repository

--- tests/repository/test_defect.py
+++ tests/repository/test_defect.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import Mock
 from dormyboba_core import entity
+from dormyboba_core.entity.defect import DefectStatus, DefectType
 from gspread import Worksheet
 from dormyboba_core.repository import GsheetDefectRepository
 from dormyboba_core.entity import Defect
@@ -10,6 +11,19 @@ class TestGsheetDefectRepository(unittest.TestCase):
     def setUp(self):
         self.mock_worksheet = Mock(spec=Worksheet)
         self.repository = GsheetDefectRepository(self.mock_worksheet)
+
+    def test_update_nonexistent_defect(self):
+        defect = Defect(
+            defect_id='1',
+            user_id=1,
+            description='sample desc',
+            defect_type=DefectType.ELECTRICITY,
+            defect_status=DefectStatus.CREATED,
+        )
+        self.mock_worksheet.col_values.return_value = [['1', 2, '3', '4', '5']]
+        self.mock_worksheet.range.return_value = [['1', 2, '3', '4', '5']]
+        with self.assertRaises(ValueError):
+            self.repository.update(defect)
 
     def test_get_defect_by_nonexistent_id(self):
         self.mock_worksheet.col_values.return_value = [['1', 2, '3', '4', '5']]
