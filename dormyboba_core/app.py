607423ca4c1b78669172cc982937ca1c5e8bcca0
Artemy Pozdnyakov <aapozdnyakove@gmail.com>
2024-02-27 17:21:38 +0300

    [FEAT-3] Implement healthcheck (#27)
    * Add healthcheck
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>
    
    * Add HEALTHCHECK docker command
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>
    
    ---------
    
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- dormyboba_core/app.py
+++ dormyboba_core/app.py
@@ -8,6 +8,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from dependency_injector.wiring import Provide
+from pydantic import BaseModel
 from .container import Container
 from .server import DormybobaServer
 from .logger import setup_logging
@@ -72,3 +73,28 @@ async def invite_register_user(req: InviteRegisterUserRequest):
     ))
     logger.info(req)
     return
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "OK"
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+    return HealthCheck(status="OK")
