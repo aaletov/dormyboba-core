f9b645f9b16d5c70efb998387db7933d6c6960cb
aapozd <aapozdnyakove@gmail.com>
2024-03-09 23:36:47 +0000

    Containers are mapped to localhost
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- tests/integration/features/environment.py
+++ tests/integration/features/environment.py
@@ -3,8 +3,8 @@ import grpc
 import sqlalchemy
 import dormyboba_api.v1api_pb2_grpc as apiv1grpc
 
-TEST_CORE_ADDR = "dormyboba_core:50051"
-TEST_DB_ADDR = f"postgresql+psycopg2://postgres:123456@postgresql/dormyboba"
+TEST_CORE_ADDR = "localhost:50051"
+TEST_DB_ADDR = f"postgresql+psycopg2://postgres:123456@localhost:5432/dormyboba"
 
 def before_all(context: behave_runner.Context):
     channel = grpc.aio.insecure_channel(TEST_CORE_ADDR)
