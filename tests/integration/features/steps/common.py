2bf517c7b18db7521edfe5f9f0982552f722bab5
aapozd <aapozdnyakove@gmail.com>
2024-03-17 15:32:54 +0000

    Implement new user steps
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- tests/integration/features/steps/common.py
+++ tests/integration/features/steps/common.py
@@ -1,7 +1,7 @@
 import json
 import datetime
 import behave.runner as behave_runner
-from behave import given, when, then
+from behave import use_step_matcher, given, when, then
 from sqlalchemy import Engine, select
 from sqlalchemy.orm import Session
 from google.protobuf.timestamp_pb2 import Timestamp
@@ -27,12 +27,39 @@ class DormybobaRole(BaseModel):
     def to_model(self) -> model.DormybobaRole:
         return model.DormybobaRole(role_id=self.role_id, role_name=self.role_name)
 
-@given(u'В базе есть роль "{role}"')
+    def to_api(self) -> apiv1.DormybobaRole:
+        return apiv1.DormybobaRole(role_id=self.role_id, role_name=self.role_name)
+
+@given(u'в базе есть роль "{role}"')
 def step_impl(context: behave_runner.Context, role: str):
     spec = DormybobaRole(**json.loads(context.text))
     engine: Engine = context.engine
     with Session(engine) as session, session.begin():
         session.add(spec.to_model())
+
+class DormybobaUser(BaseModel):
+    user_id: int
+    role: DormybobaRole
+
+    def to_model(self) -> model.DormybobaUser:
+        return model.DormybobaUser(
+            user_id=self.user_id,
+            role=self.role.to_model(),
+        )
+
+    def to_api(self) -> apiv1.DormybobaUser:
+        return apiv1.DormybobaUser(user_id=self.user_id, role=self.role.to_api())
+
+use_step_matcher("re")
+
+@given(u'в базе есть пользователь(?P<anything>.*)')
+def step_impl(context: behave_runner.Context, anything: str):
+    spec = DormybobaUser(**json.loads(context.text))
+    engine: Engine = context.engine
+    with Session(engine) as session, session.begin():
+        session.add(spec.to_model())
+
+use_step_matcher("parse")
 
 def dt_to_timestamp(dt: datetime.datetime | None) -> Timestamp:
     if dt is None:
