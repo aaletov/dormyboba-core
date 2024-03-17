6b601be676e82acf7ab8536416a3cde850c3c94d
aapozd <aapozdnyakove@gmail.com>
2024-03-17 22:36:07 +0000

    Fix institute convertsion
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- tests/integration/features/steps/common.py
+++ tests/integration/features/steps/common.py
@@ -94,7 +94,7 @@ class Institute(BaseModel):
     def to_model(self) -> model.Institute:
         return model.Institute(institute_id=self.institute_id, institute_name=self.institute_name)
 
-    def to_model(self) -> apiv1.Institute:
+    def to_api(self) -> apiv1.Institute:
         return apiv1.Institute(institute_id=self.institute_id, institute_name=self.institute_name)
 
 @given(u'в базе есть институт "{institute_name}"')
